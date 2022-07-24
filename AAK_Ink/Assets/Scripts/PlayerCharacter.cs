22d7d58753eac8d8708dc2ab3f4c0d64d90c4a1c
Schossi <reinhardschossleitner@gmail.com>
2022-07-24 10:26:54 +0200

    basic AAK setup

---
+++ AAK_Ink/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,37 @@
+using AdventureCore;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PlayerCharacter : CharacterBaseTyped<MinimalCharacterActor, CharacterControllerMovement, ListedInventory>
+{
+    [Header("Interaction")]
+    public CharacterActionArea InteractionArea;
+    public GameObject InteractionUI;
+    public TMPro.TMP_Text InteractionText;
+
+    protected override void Start()
+    {
+        InteractionArea.Changed.AddListener(new UnityAction(interactionChanged));
+        interactionChanged();
+    }
+    
+    public void OnConfirm(InputValue _)
+    {
+        if (!Actor.IsActing && InteractionArea.CanStart)
+            InteractionArea.StartAction();
+        else
+            Actor.OnInput(true);
+    }
+    public void OnCancel(InputValue _)
+    {
+        Actor.OnInput(false);
+    }
+
+    private void interactionChanged()
+    {
+        InteractionUI.SetActive(InteractionArea.CanStart);
+        InteractionText.text = InteractionArea.Text;
+    }
+}
